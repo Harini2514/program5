ead4c4a2effcea03c0e7bc32414345b0f0104748
MERLIN HARINI A <harinimerlin2@gmail.com>
2026-09-25 12:45:05 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,11 @@
+use merlin063;
+INSERT INTO Stutent(stutentID,stutentName,Gender,DeartmentID)
+VALUES
+(1001, 'Arun', 'Male',101),
+(1003, 'Divya', 'Female', 102),
+(1003, 'Karthik', 'Male', 103);
+SELECT * FROM stutent;
+desc stutent;
+SHOW CREATE TABLE stutent;
+
 
