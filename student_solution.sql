7aa5117ae5e7306f4b27f2df426f85e7a4c6bfeb
MERLIN HARINI A <harinimerlin2@gmail.com>
2026-09-25 12:52:09 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,11 +1,12 @@
 use merlin063;
-INSERT INTO Stutent(stutentID,stutentName,Gender,DeartmentID)
-VALUES
-(1001, 'Arun', 'Male',101),
-(1003, 'Divya', 'Female', 102),
-(1003, 'Karthik', 'Male', 103);
-SELECT * FROM stutent;
-desc stutent;
-SHOW CREATE TABLE stutent;
+USE CollegeDB;
+
+INSERT INTO Student VALUES (...);
+INSERT INTO Student VALUES (...);
+INSERT INTO Student VALUES (...);
+INSERT INTO Student VALUES
+(1, 'Arun', 'CSE', 20),
+(2, 'Divya', 'ECE', 20),
+(3, 'Karthik', 'IT', 21);
 
 
